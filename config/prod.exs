97949b22845514f17e0ab7ddc383a6d2764b3522
ABuffSeagull <reecevanatta@gmail.com>
2019-01-11 17:09:23 -0500

    Check origin on web socket

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,8 @@ use Mix.Config
 # before starting your production server.
 config :bingo, BingoWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "example.com", port: 80],
+  url: [host: "${PROJECT_URL}.gigalixirapp.com"],
+  check_origin: ["//${PROJECT_URL}.gigalixirapp.com", "//localhost"],
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true,
   secret_key_base: "${SECRET_KEY_BASE}"
